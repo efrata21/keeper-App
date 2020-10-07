7790c9292a77ae8cb7341d36a287b3631259a3bf
efratamillion <efratamillion@gmail.com>
2020-10-07 09:15:01 -0700

    First commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1>This is title the for the note</h1>
+      <p>This is note </p>
+    </div>
+  );
+}
+
+export default Note;
