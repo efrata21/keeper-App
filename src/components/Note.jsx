3570cb5073df0b3d07992a2adc627f1e7c2d0644
efrata21 <72409910+efrata21@users.noreply.github.com>
2020-10-07 05:38:02 -0700

    Update Note.jsx
    adding the css class

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Note() {
   return (
-    <div>
+    <div className="note" >
       <h1>This is title the for the note</h1>
       <p>This is note </p>
     </div>
